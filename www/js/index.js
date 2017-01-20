a07955827d5b4514f466c7d78fb742c2dc2a0034
longvhv <long@vhv.vn>
2017-01-20 08:00:10 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -58,7 +58,11 @@ var app = {
             "windows": {}
         });
         //console.log('after init');
-
+	var notFirstTime = localStorage.getItem('notFirstTime');
+	if(!notFirstTime)
+	{
+		localStorage.setItem('notFirstTime', 1);
+	}
         push.on('registration', function(data) {
             //console.log('registration event: ' + data.registrationId);
 
@@ -69,7 +73,10 @@ var app = {
                 // Post registrationId to your app server as the value has changed
             }
 			app.registrationId = data.registrationId;
+		if(!notFirstTime)
+		{
 			var ref = window.open('http://quaxanh.top/?page=Mobile.home&androidRegistrationId='+data.registrationId, '_blank', 'fullscreen=yes,location=no,zoom=no,status=no,toolbar=no,titlebar=no,disallowoverscroll=true');
+		}
 		document.getElementById('img1').onclick = document.getElementById('img2').onclick = document.getElementById('img3').onclick = function() {
 			
 		var ref = window.open('http://quaxanh.top/?page=Mobile.home&androidRegistrationId='+data.registrationId, '_blank', 'fullscreen=yes,location=no,zoom=no,status=no,toolbar=no,titlebar=no,disallowoverscroll=true');
@@ -78,7 +85,10 @@ var app = {
 
         push.on('error', function(e) {
             //console.log("push error = " + e.message);
+		if(!notFirstTime)
+		{
 			var ref = window.open('http://quaxanh.top/?page=Mobile.home', '_blank', 'fullscreen=yes,location=no,zoom=no,status=no,toolbar=no,titlebar=no,disallowoverscroll=yes');
+		}
 		document.getElementById('img1').onclick = document.getElementById('img2').onclick = document.getElementById('img3').onclick = function() {
 			var ref = window.open('http://quaxanh.top/?page=Mobile.home', '_blank', 'fullscreen=yes,location=no,zoom=no,status=no,toolbar=no,titlebar=no,disallowoverscroll=yes');
 		};
